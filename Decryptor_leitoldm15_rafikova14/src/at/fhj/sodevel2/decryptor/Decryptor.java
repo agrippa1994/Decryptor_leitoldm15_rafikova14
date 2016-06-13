670f26c626c6c65ce3ddce0feb18f569d93cd37d
agrippa1994 <agrippa1994@icloud.com>
2016-06-13 12:47:48 +0200

    Erster Upload

---
+++ Decryptor_leitoldm15_rafikova14/src/at/fhj/sodevel2/decryptor/Decryptor.java
@@ -0,0 +1,14 @@
+package at.fhj.sodevel2.decryptor;
+
+public class Decryptor {
+	private String input;
+	
+	public Decryptor(String input) {
+		this.input = input;
+	}
+
+	public String[] getResults() {
+		
+	}
+	
+}
---
+++ Decryptor_leitoldm15_rafikova14/src/at/fhj/sodevel2/decryptor/DecryptorMain.java
@@ -0,0 +1,10 @@
+package at.fhj.sodevel2.decryptor;
+
+public class DecryptorMain {
+
+	public static void main(String[] args) {
+		Decryptor d = new Decryptor("fhqu qnf Jneffreenq fb svaqrfg qh nhqu qra pnpu tyrvpu");
+		String[] results = d.getResults();
+	}
+
+}
